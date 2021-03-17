4517c8d54163f0a1fd6bc3daa526de42b18cd648
Refayat Haque <rhaque@pyramidsystems.com>
2021-03-16 23:41:43 -0400

    Tue Mar 16 23:41

--- blog_post_material/blog_post_1/output.tf
+++ blog_post_material/blog_post_1/output.tf
@@ -10,18 +10,14 @@ output "bucket_website_endpoint" {
   value = aws_s3_bucket.website_static_files.website_endpoint
 }
 
-output "test1" {
+output "route_53_hosted_zone" {
   value = aws_s3_bucket.website_static_files.hosted_zone_id
 }
 
-output "test2" {
-  value = aws_acm_certificate.certificate.domain_validation_options
-}
-
-output "test3" {
+output "certificate_arn" {
   value = aws_acm_certificate.certificate.arn
 }
 
-# output "test4" {
-#   value = aws_route53_record.certificate_validation.fqdn
-# }
+output "certificate_domain_validation_options" {
+  value = aws_acm_certificate.certificate.domain_validation_options
+}
